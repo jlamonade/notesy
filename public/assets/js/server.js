b88ca3c17c61e416553a78f8bc24496e1e0b0bfc
Jason Lam <nyjasonlam@gmail.com>
2021-05-12 20:05:39 -0400

    Added index.html and notes.html routes

--- public/assets/js/server.js
+++ public/assets/js/server.js
@@ -11,4 +11,12 @@ app.get("/", (req, res) =>
   res.sendFile(path.join(__dirname, "../../index.html"))
 );
 
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "../../notes.html"))
+);
+
+app.get("/api/notes", (req, res) => res.sendFile(path.join(__dirname, "../../../db/db.json")));
+
+
+
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
