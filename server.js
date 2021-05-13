66047fdb7deb9dc7189b70f5b4e7311c1881363d
Jason Lam <nyjasonlam@gmail.com>
2021-05-12 21:07:46 -0400

    Added route to save notes

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'))
+app.use(express.static("public"));
 app.use(express.json());
 
 // route to root
@@ -26,8 +27,12 @@ app.get("/api/notes", (req, res) =>
 // route to write notes
 app.post("/api/notes", (req, res) => {
   const note = req.body;
-
-  console.log(note);
+  fs.readFile("./db/db.json", (err, data) => {
+    if (err) throw err;
+    let notesArr = JSON.parse(data);
+    notesArr.push(note);
+    fs.writeFile('./db/db.json', JSON.stringify(notesArr), (err) => {if (err) throw err});
+  });
 });
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
